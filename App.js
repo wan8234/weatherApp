2d1a3ca4fbc7bd3bc8a776271133cdbc484c0579
wan8234 <wan8234@naver.com>
2021-01-18 01:31:21 +0900

    make content box

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Component} from 'react';
-import {View, TouchableOpacity, Image} from 'react-native';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
 
 var HAMBURGER = 1;
 var MUSIC = 2;
+var SUNNY = 10;
 
 class ImageButton extends Component {
     constructor(props) {
@@ -28,6 +29,25 @@ class ImageButton extends Component {
                 <TouchableOpacity>{imageComponent}</TouchableOpacity>
             </View>
         );
+    }
+}
+
+class Content extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        var imageComponent =
+            this.props.btnType == SUNNY ? (
+                <Image
+                    style={styles.weatherIcon}
+                    source={require('./resource/Sun.png')}
+                />
+            ) : (
+                <Image></Image>
+            );
+        return <View style={this.props.style}>{imageComponent}</View>;
     }
 }
 
@@ -62,21 +82,65 @@ export default class App extends Component {
 
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    alignItems: 'center',
-                    backgroundColor: '#AF1423',
-                }}>
-                <ImageButton
-                    style={{width: 100, height: 100, resizeMode: 'contain'}}
-                    btnType={HAMBURGER}
-                />
-                <ImageButton
-                    style={{width: 100, height: 100, resizeMode: 'contain'}}
-                    btnType={MUSIC}
-                />
-            </View>
+            <>
+                <Image
+                    source={require('./resource/background.jpg')}
+                    style={styles.backgroundJPG}></Image>
+                <View style={styles.navbar}>
+                    <ImageButton
+                        style={{width: 25, height: 25, resizeMode: 'contain'}}
+                        btnType={HAMBURGER}
+                    />
+                    <Text style={styles.navDate}>October 20th</Text>
+                    <ImageButton
+                        style={{width: 28, height: 28, resizeMode: 'contain'}}
+                        btnType={MUSIC}
+                    />
+                </View>
+                <Content style={styles.mainContent} btnType={SUNNY}></Content>
+                <Content style={styles.blank}></Content>
+            </>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    navbar: {
+        flex: 0.4,
+        alignItems: 'flex-start',
+        justifyContent: 'space-around',
+        backgroundColor: '#AF1423',
+        flexDirection: 'row',
+        paddingTop: 10,
+    },
+    navDate: {
+        paddingRight: 50,
+        paddingLeft: 50,
+        fontSize: 20,
+    },
+    mainContent: {
+        flex: 2,
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 10,
+        marginBottom: 10,
+        backgroundColor: '#BEEBE9',
+        opacity: 0.5,
+        borderRadius: 20,
+    },
+    blank: {
+        flex: 5,
+    },
+    backgroundJPG: {
+        position: 'absolute',
+        zIndex: -1,
+        width: '100%',
+        height: '100%',
+    },
+    weatherIcon: {
+        flex: 1,
+        width: 110,
+        height: 110,
+        resizeMode: 'contain',
+    },
+});
